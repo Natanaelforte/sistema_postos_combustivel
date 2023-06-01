ddd176e581015a5cb111234fa4949be642f0c68b
MAGELA PINHEIRO <magela.pinheiro@m2atecnologia.com.br>
2023-06-01 17:43:44 -0300

    estilizando campos do form de login

--- acesso/forms.py
+++ acesso/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from django import forms
 
 from posto.models import Posto
@@ -6,6 +6,7 @@ from posto.models import Posto
 
 class AcessoForm(AuthenticationForm):
     posto = forms.ModelChoiceField(queryset=Posto.objects.none(), widget=forms.Select(), required=False)
+    username = UsernameField(widget=forms.TextInput(attrs={"autofocus": True, 'class': 'ap-input-login', 'placeholder': 'Usuário'}))
 
     def __init__(self, request=None, *args, **kwargs):
         super(AcessoForm, self).__init__(request, *args, **kwargs)
